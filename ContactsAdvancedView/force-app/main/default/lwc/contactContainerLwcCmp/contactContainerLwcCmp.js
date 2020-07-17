d7c84984ed7fa3df428bfdf267a74876f461c8ac
sudikrt <sudarshankaranth18@gmail.com>
2020-07-17 22:14:09 +0530

    comment by sudarshan

--- ContactsAdvancedView/force-app/main/default/lwc/contactContainerLwcCmp/contactContainerLwcCmp.js
+++ ContactsAdvancedView/force-app/main/default/lwc/contactContainerLwcCmp/contactContainerLwcCmp.js
@@ -3,4 +3,5 @@ import { LightningElement } from 'lwc';
 export default class contactContainerLwcCmp extends LightningElement {
   greeting = 'World';
   message = 'mera bharat mahaan';
+  message = 'Some text added'
 }
