6434df493871ee254a96f0837feb8f96e475f9a2
Yogesh Nama <yogeshnm59@gmail.com>
2020-07-17 22:03:34 +0530

    msgChangedtoPakistan

--- ContactsAdvancedView/force-app/main/default/lwc/contactContainerLwcCmp/contactContainerLwcCmp.js
+++ ContactsAdvancedView/force-app/main/default/lwc/contactContainerLwcCmp/contactContainerLwcCmp.js
@@ -2,5 +2,5 @@ import { LightningElement } from 'lwc';
 
 export default class contactContainerLwcCmp extends LightningElement {
   greeting = 'World';
-  message = 'Namaste India';
+  message = 'Namaste Pakistan';
 }
